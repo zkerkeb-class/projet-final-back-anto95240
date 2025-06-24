10e426ea63bfa7dc2d21e713f4584e5c1d7c8af9
Antoine RICHARD <112949963+anto95240@users.noreply.github.com>
2025-06-24 16:05:17 +0200

    Update verifytoken.js

--- src/middlewares/verifytoken.js
+++ src/middlewares/verifytoken.js
@@ -10,7 +10,6 @@ const verifyToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("decoded token:", decoded);
 
     req.userId = decoded.uid;
     
